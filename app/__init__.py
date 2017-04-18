1cc5e4f22d9c4ce925f8fa0d21520d7fd547dd6f
王瑞东 <ruidong.wang@tsingdata.com>
2017-04-18 15:52:32 +0800

    #8 supervisor多进程管理 nginx负载均衡配置完成

--- app/__init__.py
+++ app/__init__.py
@@ -49,7 +49,7 @@ app.json_encoder = CustomJSONEncoder
 if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/microblog.log', 'a',
+    file_handler = RotatingFileHandler('tmp/web_log.log', 'a',
                                        1 * 1024 * 1024, 10)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter(
